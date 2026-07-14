1ae26d6c6e77b533219444d8d71bf6e12ddc4e03
ismayildasdemirli27 <dasdemirliismayil40@gmail.com>
2026-07-14 17:00:09 +0000

    Task 12: Fix Pycodestyle line length

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -11,9 +11,10 @@ def roman_to_int(roman_string):
     length = len(roman_string)
 
     for i in range(length):
-        if i < length - 1 and roman_dict.get(roman_string[i], 0) < roman_dict.get(roman_string[i+1], 0):
-            num -= roman_dict.get(roman_string[i], 0)
+        val = roman_dict.get(roman_string[i], 0)
+        if i < length - 1 and val < roman_dict.get(roman_string[i+1], 0):
+            num -= val
         else:
-            num += roman_dict.get(roman_string[i], 0)
+            num += val
 
     return num
