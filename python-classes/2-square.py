7432ef4ca27d47015d9757cc42d160a7952199e9
ismayildasdemirli27 <dasdemirliismayil40@gmail.com>
2026-07-15 10:04:42 +0000

    Fix type validation to use isinstance

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -12,13 +12,10 @@ class Square:
     def __init__(self, size=0):
         """
         Initializes a new Square.
-
-        Args:
-            size (int): The size of the new square. Defaults to 0.
         """
-        if type(size) is not int:
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
-        
+
         self.__size = size
