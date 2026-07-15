14f9b83c1365773a8fcd5061fc265673a3afdb17
ismayildasdemirli27 <dasdemirliismayil40@gmail.com>
2026-07-15 14:45:34 +0000

    Update Task 6: Add module and class documentation, implement number_of_instances

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -1,1 +1,27 @@
 #!/usr/bin/python3
+"""
+This module defines a Rectangle class.
+It demonstrates the use of class attributes and instance deletion.
+"""
+
+
+class Rectangle:
+    """
+    Rectangle class with private instance attributes width and height,
+    and a public class attribute number_of_instances.
+    """
+    
+    number_of_instances = 0
+
+    def __init__(self, width=0, height=0):
+        """Initializes a new Rectangle instance."""
+        self.width = width
+        self.height = height
+        Rectangle.number_of_instances += 1
+
+    # ... Sizin əvvəlki tapşırıqlardan qalan metodlarınız (area, perimeter, __str__, __repr__ və s.) ...
+
+    def __del__(self):
+        """Prints a message and decrements instance count upon deletion."""
+        print("Bye rectangle...")
+        Rectangle.number_of_instances -= 1
