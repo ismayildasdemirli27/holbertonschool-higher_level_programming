1f6e286b17bf66bb45cd4249328b52350d5b43f9
ismayildasdemirli27 <dasdemirliismayil40@gmail.com>
2026-07-18 12:35:16 +0000

    1. Write to a file

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+This module provides a function for writing text to a file.
+"""
+
+
+def write_file(filename="", text=""):
+    """
+    Writes a string to a text file (UTF8) and returns the number
+    of characters written.
+    
+    Args:
+        filename (str): The path to the file to write to.
+        text (str): The content to write into the file.
+        
+    Returns:
+        int: The number of characters successfully written.
+    """
+    with open(filename, mode='w', encoding='utf-8') as f:
+        return f.write(text)
