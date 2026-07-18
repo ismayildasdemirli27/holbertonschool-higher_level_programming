6485f66c351d1de1bd26cc6e7bcd6ed8279249ed
ismayildasdemirli27 <dasdemirliismayil40@gmail.com>
2026-07-18 12:32:38 +0000

    Add README and complete read_file function

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 """
-Module that reads a text file and prints it to stdout.
+This module contains a function that reads a text file and prints it to stdout.
 """
 
+
 def read_file(filename=""):
-    """Reads a text file (UTF8) and prints it to stdout."""
+    """
+    Reads a text file (UTF8) and prints it to stdout.
+    Must use the 'with' statement.
+    """
     with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")
