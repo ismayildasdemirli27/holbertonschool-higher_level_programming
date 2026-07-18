96aabe9a87393973e0c6cfd5f846ba55d3701d35
ismayildasdemirli27 <dasdemirliismayil40@gmail.com>
2026-07-18 13:17:53 +0000

    12. Pascal's Triangle

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Module for generating Pascal's triangle."""
+
+
+def pascal_triangle(n):
+    """Returns a list of lists representing Pascal's triangle of n."""
+    if n <= 0:
+        return []
+
+    triangle = []
+    for i in range(n):
+        row = [1] * (i + 1)
+        for j in range(1, i):
+            row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
+        triangle.append(row)
+
+    return triangle
