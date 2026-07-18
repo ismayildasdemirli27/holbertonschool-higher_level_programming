2eb18d778e78e8a3ffa96bf184a5337692ea67f9
ismayildasdemirli27 <dasdemirliismayil40@gmail.com>
2026-07-18 12:52:08 +0000

    Fix PEP8 trailing whitespace again

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -1,20 +1,8 @@
 #!/usr/bin/python3
-"""
-This module provides a function that returns an object
-represented by a JSON string.
-"""
+"""This module provides a function to convert a JSON string to an object."""
 import json
 
 
 def from_json_string(my_str):
-    """
-    Returns an object (Python data structure) represented by
-    a JSON string.
-    
-    Args:
-        my_str (str): The JSON string to deserialize.
-        
-    Returns:
-        The Python object represented by the JSON string.
-    """
+    """Returns an object represented by a JSON string."""
     return json.loads(my_str)
