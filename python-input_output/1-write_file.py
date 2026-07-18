e4b5f9e9b9948b81c8b16ca89c98e6f0fdf3be9a
ismayildasdemirli27 <dasdemirliismayil40@gmail.com>
2026-07-18 12:37:49 +0000

    Fix PEP8 validation

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -8,11 +8,9 @@ def write_file(filename="", text=""):
     """
     Writes a string to a text file (UTF8) and returns the number
     of characters written.
-    
     Args:
         filename (str): The path to the file to write to.
         text (str): The content to write into the file.
-        
     Returns:
         int: The number of characters successfully written.
     """
