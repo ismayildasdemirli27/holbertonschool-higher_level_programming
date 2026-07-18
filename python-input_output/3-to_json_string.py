2dd43ff3a6820223168f159c4e0ebb2b7cc64d74
ismayildasdemirli27 <dasdemirliismayil40@gmail.com>
2026-07-18 12:44:40 +0000

    Fix PEP8 whitespace in docstring

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -9,10 +9,8 @@ import json
 def to_json_string(my_obj):
     """
     Returns the JSON representation of an object (string).
-    
     Args:
         my_obj: The object to be serialized into JSON.
-        
     Returns:
         str: The JSON string representation of the object.
     """
