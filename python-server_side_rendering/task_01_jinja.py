03d6967da45f536023bb2d53bfadfd20badc4b4c
ismayildasdemirli27 <dasdemirliismayil40@gmail.com>
2026-07-21 14:35:24 +0000

    Task 1: Implement Flask app with routes for templates

---
+++ python-server_side_rendering/task_01_jinja.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Task 1: Creating a Basic HTML Template in Flask
+"""
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    """Render the home page."""
+    return render_template('index.html')
+
+
+@app.route('/about')
+def about():
+    """Render the about page."""
+    return render_template('about.html')
+
+
+@app.route('/contact')
+def contact():
+    """Render the contact page."""
+    return render_template('contact.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
