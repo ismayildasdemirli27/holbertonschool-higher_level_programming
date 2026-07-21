b85eb5a5764e6d9b28a740ffb661fd1eb28e800d
Ismayil <dasdemirliismayil40@gmail.com>
2026-07-21 05:08:50 +0400

    Delete 1-create_user.sql

--- 1-create_user.sql
+++
@@ -1,4 +0,0 @@
--- Create the user if it doesn't exist
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
--- Set privileges
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
