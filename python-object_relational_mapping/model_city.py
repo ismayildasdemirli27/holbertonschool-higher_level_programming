d37d05155b06e616627c6cc20ea8872ca7cb16fe
ismayildasdemirli27 <dasdemirliismayil40@gmail.com>
2026-07-21 12:49:57 +0000

    Task 14: Add relationship to City model for state navigation

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -2,7 +2,7 @@
 """
 Contains the class definition of a City.
 """
-from model_state import Base
+from model_state import Base, State
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
 
@@ -14,3 +14,4 @@ class City(Base):
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
+    state = relationship("State")
