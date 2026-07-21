b5252deee76f22d7b2f5495562f05b501db733e0
ismayildasdemirli27 <dasdemirliismayil40@gmail.com>
2026-07-21 11:09:47 +0000

    Task 1: Final clean version with strict PEP8

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -6,7 +6,6 @@ from the database hbtn_0e_0_usa.
 import sys
 import MySQLdb
 
-
 if __name__ == "__main__":
     db = MySQLdb.connect(
         host="localhost",
@@ -15,14 +14,10 @@ if __name__ == "__main__":
         passwd=sys.argv[2],
         db=sys.argv[3]
     )
-    
     cur = db.cursor()
     cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' "
-                "ORDER BY id ASC")
-    
-    query_rows = cur.fetchall()
-    for row in query_rows:
+                "ORDER BY states.id ASC")
+    for row in cur.fetchall():
         print(row)
-        
     cur.close()
     db.close()
