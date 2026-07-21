4816e2bbc183a96202ea6e9f45d0ab900a57f99c
Ismayil <dasdemirliismayil40@gmail.com>
2026-07-21 05:10:00 +0400

    Delete 6-states.sql

--- 6-states.sql
+++
@@ -1,7 +0,0 @@
--- example
-CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-USE hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS states (
-    id INT AUTO_INCREMENT NOT NULL UNIQUE PRIMARY KEY,
-    name VARCHAR(256)
-);
