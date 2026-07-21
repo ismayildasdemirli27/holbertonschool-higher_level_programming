f89a0a7734be61f9969a00de74ed07a0407507d0
ismayildasdemirli27 <dasdemirliismayil40@gmail.com>
2026-07-21 12:32:14 +0000

    Task 9: List states containing the letter 'a' via SQLAlchemy

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,29 +1,28 @@
 #!/usr/bin/python3
 """
-List all State objects from the database
+Lists all State objects that contain the letter a
+from the database hbtn_0e_6_usa.
 """
-
-from sys import argv
+import sys
+from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
 
 if __name__ == "__main__":
-    usnm = argv[1]
-    pwd = argv[2]
-    db = argv[3]
-
-    engine = create_engine(f"mysql+mysqldb://{usnm}:{pwd}@localhost:3306/{db}")
-
+    # Verilənlər bazasına qoşuluruq
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    states = (
-      session.query(State)
-      .filter(State.name.like('%a%'))
-      .order_by(State.id)
-      .all()
-    )
+    # İçində 'a' hərfi olanları filtrləyirik və id-yə görə sıralayırıq
+    states = session.query(State).filter(
+        State.name.like('%a%')
+    ).order_by(State.id)
 
+    # Nəticələri tələb olunan formatda çap edirik
     for state in states:
-        print(f"{state.id}: {state.name}")
+        print("{}: {}".format(state.id, state.name))
+        
+    session.close()
