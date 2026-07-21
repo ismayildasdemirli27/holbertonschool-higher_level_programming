e200f4f1fd26b1c94baf8bda703a22f24d2f37a8
ismayildasdemirli27 <dasdemirliismayil40@gmail.com>
2026-07-21 12:26:02 +0000

    Task 7: Fetch all states via SQLAlchemy

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,24 +1,24 @@
 #!/usr/bin/python3
 """
-List all State objects from the database
+Lists all State objects from the database hbtn_0e_6_usa.
 """
-
-from sys import argv
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+import sys
 from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
-    usnm = argv[1]
-    pwd = argv[2]
-    db = argv[3]
-
-    engine = create_engine(f"mysql+mysqldb://{usnm}:{pwd}@localhost:3306/{db}")
-
+    # Verilənlər bazasına qoşuluruq (pool_pre_ping=True mütləqdir)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    
+    # Sessiya yaradırıq
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    states = session.query(State).order_by(State.id).all()
-
-    for state in states:
-        print(f"{state.id}: {state.name}")
+    # Bütün State obyektlərini id-yə görə artan ardıcıllıqla çəkib çap edirik
+    for state in session.query(State).order_by(State.id).all():
+        print("{}: {}".format(state.id, state.name))
+        
+    # Sessiyanı bağlayırıq
+    session.close()
