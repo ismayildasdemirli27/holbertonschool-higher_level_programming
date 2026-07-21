241488e80b3baddded5f042936bb6b84802f0b44
ismayildasdemirli27 <dasdemirliismayil40@gmail.com>
2026-07-21 11:29:10 +0000

    Task 4: List all cities by state using JOIN

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -1,45 +1,30 @@
 #!/usr/bin/python3
 """
-this is global enviroment
+Lists all cities from the database hbtn_0e_4_usa.
 """
-
-
 import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    """
-        this is local enviroment
-    """
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-    state = sys.argv[4]
-
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
-        user=username,
-        passwd=password,
-        db=database
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        charset="utf8"
     )
-
-    cursor = db.cursor()
-
-    query = """
-        SELECT cities.name
-        FROM cities
-        INNER JOIN states ON cities.state_id = states.id
-        WHERE states.name = %s
-        ORDER BY cities.id ASC
-        """
-    cursor.execute(query, (state, ))
-
-    states = cursor.fetchall()
-    states_new = []
-    for state in states:
-        states_new.append(state[0])
-    print(", ".join(states_new))
-
-    cursor.close()
+    cur = db.cursor()
+    
+    # İki cədvəli JOIN edirik və cities.id-yə görə sıralayırıq
+    query = ("SELECT cities.id, cities.name, states.name "
+             "FROM cities "
+             "JOIN states ON cities.state_id = states.id "
+             "ORDER BY cities.id ASC")
+    cur.execute(query)
+    
+    for row in cur.fetchall():
+        print(row)
+        
+    cur.close()
     db.close()
