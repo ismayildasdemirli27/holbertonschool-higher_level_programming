3e5f9f0657cd08ffc65f87ccaa6a41fd2c3a56bf
ismayildasdemirli27 <dasdemirliismayil40@gmail.com>
2026-07-21 11:07:46 +0000

    Task 1: Fix pycodestyle line length issue

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,42 +1,3 @@
-<<<<<<< HEAD
-#!/usr/bin/python3
-"""
-this is global enviroment
-"""
-
-
-import sys
-import MySQLdb
-
-if __name__ == "__main__":
-    """
-        this is local enviroment
-    """
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=username,
-        passwd=password,
-        db=database
-    )
-
-    cursor = db.cursor()
-
-    query = 'SELECT * FROM states WHERE BINARY name LIKE "N%";'
-    cursor.execute(query)
-
-    states = cursor.fetchall()
-
-    for state in states:
-        print(state)
-
-    cursor.close()
-    db.close()
-=======
 #!/usr/bin/python3
 """
 Lists all states with a name starting with N (upper N)
@@ -44,6 +5,7 @@ from the database hbtn_0e_0_usa.
 """
 import sys
 import MySQLdb
+
 
 if __name__ == "__main__":
     db = MySQLdb.connect(
@@ -55,8 +17,8 @@ if __name__ == "__main__":
     )
     
     cur = db.cursor()
-    # BINARY açar sözü yalnız böyük N hərfi ilə başlayanları tapmağı təmin edir
-    cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC")
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' "
+                "ORDER BY id ASC")
     
     query_rows = cur.fetchall()
     for row in query_rows:
@@ -64,4 +26,3 @@ if __name__ == "__main__":
         
     cur.close()
     db.close()
->>>>>>> 2fdd82f (Task 1: Filter states strictly starting with upper N)
