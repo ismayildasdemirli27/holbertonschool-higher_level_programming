a24e55a161c5f9d1a7b6f24b48a6df7032c8f025
ismayildasdemirli27 <dasdemirliismayil40@gmail.com>
2026-07-21 12:41:25 +0000

    Task 12: Update state with id 2 to New Mexico

--- python-object_relational_mapping/12-model_state_update_id_2.py
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,30 +1,26 @@
 #!/usr/bin/python3
 """
-List all State objects from the database
+Changes the name of a State object from the database hbtn_0e_6_usa.
 """
-
-from sys import argv
+import sys
+from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
 
 if __name__ == "__main__":
-
-    usnm = argv[1]
-    pwd = argv[2]
-    db = argv[3]
-
-    engine = create_engine(
-        f"mysql+mysqldb://{usnm}:{pwd}@localhost:3306/{db}",
-        future=True
-    )
-
-    Session = sessionmaker(bind=engine, future=True)
+    # Verilənlər bazasına qoşuluruq
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    
+    Session = sessionmaker(bind=engine)
     session = Session()
 
-    state = session.query(State).filter(State.name.like("%a%")).delete(
-        synchronize_session=False
-    )
+    # ID-si 2 olan ştatı tapırıq
+    state_to_update = session.query(State).filter(State.id == 2).first()
 
-    session.commit()
+    # Əgər tapılıbsa adını dəyişib commit edirik
+    if state_to_update:
+        state_to_update.name = "New Mexico"
+        session.commit()
+        
     session.close()
